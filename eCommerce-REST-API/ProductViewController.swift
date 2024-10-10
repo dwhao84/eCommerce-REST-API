8ee932ec3268f34a0c5b16ed5265ae08eca59c2b
Dawei Hao <dwsamurai84@gmail.com>
2024-10-10 22:23:37 +0800

    Create function for customBGColor.

--- eCommerce-REST-API/ProductViewController.swift
+++ eCommerce-REST-API/ProductViewController.swift
@@ -6,24 +6,26 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ProductViewController: UIViewController {
 
+    let productCollectionView: UICollectionView = {
+        let collectionView: UICollectionView = UICollectionView(frame: .zero)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    } ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+    }
 
-        // Do any additional setup after loading the view.
+    func setupUI () {
+        self.view.backgroundColor = Color.customBGColor
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
